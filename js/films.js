2a5a5fd3015bbfc430a3e1e49e0f0f81f584830f
Gibibi0 <anast365antonova@gmail.com>
2025-06-06 18:22:42 +0300

    a system for adding films, searching for them in the entire list and finding a random film has been installed

---
+++ js/films.js
@@ -0,0 +1,50 @@
+const filmList = document.getElementById('filmList')
+const searchInput = document.getElementById('searchInput')
+const addFilmBtn = document.getElementById('addFilmBtn')
+const randomFilmBtn = document.getElementById('randomFilmBtn')
+
+let films = []
+
+function renderList() {
+	filmList.innerHTML = ''
+
+	const search = searchInput.value.toLowerCase()
+
+	films
+	.filter(film => film.title.toLowerCase().includes(search))
+	.forEach((film, index) => {
+		const li = document.createElement('li')
+		li.textContent = film.title
+		if (film.watched) li.classList.add('watched')
+
+		li.addEventListener('click', () => {
+			films[index].watched = !films[index].watched
+			renderList()
+		})
+
+		filmList.appendChild(li)
+	})
+}
+
+addFilmBtn.addEventListener('click', () => {
+	const title = prompt('Input film name: ')
+	if (title) {
+		films.push({title, watched: false})
+		renderList()
+	}
+})
+
+searchInput.addEventListener('input', renderList)
+
+randomFilmBtn.addEventListener('click', () => {
+	const unwatched = films.filter(f => !f.watched)
+	if (unwatched.length === 0) {
+		alert('All of films are watched!')
+		return
+	}
+
+	const randomIndex = Math.floor(Math.random() * unwatched.length)
+	alert(`Lets watch: ${unwatched[randomIndex].title}`)
+})
+
+renderList()
